2ff3eebb7fd19d1e868beaf644b5236337d51a55
SaiChandra1011 <saichandra1001@gmail.com>
2024-01-21 15:52:11 +0530

    added tilt to cards

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import Tilt from "react-parallax-tilt";
+
 
 function Card(props) {
 return(
+    <Tilt glareEnable={false} tiltMaxAngleX={11} 
+        tiltMaxAngleY={13} perspective={1050} 
+        >
+
+        
     <div className="card">
     <img className="plane-img" src={props.cardimage} />
     <p className="mid">{props.cardname}</p>
     </div>
+    </Tilt>
 );
 
 
