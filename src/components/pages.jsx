28bd38ee06cf7d254981c2c7c69c04a808ad5b77
dashingvinit <21bcs7277@cuchd.in>
2024-02-06 20:47:06 +0530

    done

--- src/components/pages.jsx
+++ src/components/pages.jsx
@@ -1,32 +1,21 @@
-import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Intro from './intro.jsx';
-import Boeing from './boeing.jsx';
-import boeinginformation from '../boeinginfo.jsx';
-import Airbus from './airbus.jsx';
-import AmericanJets from './americanfighters.jsx';
-import RussianJets from './russianfighters.jsx';
+import React from 'react';
+import Home from './intro';
+import Boeing from './boeing';
+import Airbus from './airbus';
+import AmericanJets from './americanfighters';
+import RussianJets from './russianfighters';
 
 function Pages() {
   return (
     <Routes>
-      <Route path="/" element={<Intro />} />
+      <Route path="/" element={<Home />} />
       <Route path="/boeing" element={<Boeing />} />
       <Route path="/airbus" element={<Airbus />} />
-      <Route path="/americanfighters" element={<AmericanJets />} />
-      <Route path="/russianfighters" element={<RussianJets />} />
+      <Route path="/american-jets" element={<AmericanJets />} />
+      <Route path="/russian-jets" element={<RussianJets />} />
     </Routes>
   );
 }
 
-{
-  boeinginformation.map((boeingItem) => (
-    <Boeing
-      key={boeingItem.key}
-      title={boeingItem.title}
-      pictureURL={boeingItem.pictureURL}
-      content={boeingItem.content}
-    />
-  ));
-}
 export default Pages;
