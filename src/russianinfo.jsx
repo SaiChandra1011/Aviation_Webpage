620c19053f223dd91847147f01251a3a41a95df3
SaiChandra1011 <saichandra1001@gmail.com>
2024-02-26 19:09:14 +0530

    css fixes and data

--- src/russianinfo.jsx
+++ src/russianinfo.jsx
@@ -31,64 +31,70 @@ const russianjetinformation = [
     },
     {
         key :6,
+        title: "Su-34",
+        pictureURL:"https://www.airforce-technology.com/wp-content/uploads/sites/6/2017/04/Image-3-Su-34-Su-32-Fullback-Fighter-Bomber.jpg",
+        content:"The Sukhoi Su-34, a formidable twin-engine fighter-bomber, embodies Russia's prowess in modern aerial warfare. Designed for versatility, it excels in both air superiority and ground attack roles, making it a highly adaptable asset on the battlefield. Equipped with advanced avionics and radar systems, the Su-34 boasts unparalleled situational awareness and target acquisition capabilities, enabling precise engagement of both aerial and ground targets. Its impressive combat range and payload capacity allow for extended missions and heavy weapon loads, ensuring sustained firepower against enemy forces. With its rugged construction and advanced defensive systems, the Su-34 can withstand hostile environments, making it a reliable platform for various combat operations. Whether conducting precision strikes, providing close air support, or performing reconnaissance missions, the Su-34 stands as a cornerstone of Russia's aerial combat capabilities."
+    },
+    {
+        key :7,
         title: "Su-35",
         pictureURL:"https://theaviationgeekclub.com/wp-content/uploads/2020/08/Su-35.jpg",
         content:"The Sukhoi Su-35, introduced in the 2000s, is a highly advanced multirole fighter aircraft renowned for its exceptional performance, agility, and combat capabilities. With a top speed exceeding Mach 2.25, the Su-35 excels in air superiority, ground attack, and reconnaissance missions. Equipped with state-of-the-art avionics, radar systems, and a diverse array of weapons, it can engage both aerial and ground targets with precision and lethality. Its thrust-vectoring engines and advanced aerodynamics provide unmatched maneuverability, allowing it to outmaneuver and defeat adversaries in close-range dogfights. Despite its formidable capabilities, the Su-35 remains agile and versatile, making it a dominant force in modern aerial warfare."
     },
     {
-        key :7,
+        key :8,
         title: "Su-47",
         pictureURL:"https://w0.peakpx.com/wallpaper/602/68/HD-wallpaper-sukhoi-su-47-su-47-berkut-plane-sukhoi.jpg",
         content:"The Sukhoi Su-47 Berkut, also known as the S-37, is an experimental supersonic fighter aircraft developed by Sukhoi Design Bureau. Introduced in the late 1990s, the Su-47 is renowned for its forward-swept wings and advanced aerodynamic design. With a top speed exceeding Mach 2, the Su-47 showcases exceptional agility and maneuverability, making it capable of performing advanced maneuvers in both air-to-air and air-to-ground roles. Equipped with advanced avionics and weapon systems, it offers a versatile platform for various mission types, including interception, reconnaissance, and ground attack. Despite being an experimental prototype, the Su-47 serves as a testament to Russian innovation in aerospace technology."
     },
     {
-        key :8,
+        key :9,
         title: "Su-57",
         pictureURL:"https://images8.alphacoders.com/859/thumb-1920-859536.jpg",
         content:"The Sukhoi Su-57, also known as the PAK FA (Perspektivny Aviatsionny Kompleks Frontovoy Aviatsii), is a fifth-generation stealth fighter aircraft developed by Sukhoi Company. Introduced in the 2010s, the Su-57 is renowned for its advanced avionics, cutting-edge radar systems, and low observability features. With a top speed exceeding Mach 2, the Su-57 showcases exceptional speed and agility, making it a potent asset for air superiority and multi-role missions. Equipped with advanced weapon systems, including air-to-air and air-to-ground missiles, it offers a versatile platform for various combat scenarios. The Su-57 represents the pinnacle of Russian aerospace technology, incorporating state-of-the-art features to ensure dominance in modern aerial warfare."
     },
     
     {
-        key :9,
+        key :10,
         title: "Su-75",
         pictureURL:"https://sites.breakingmedia.com/uploads/sites/3/2021/07/youtubeCheckmate-1024x461.png",
         content:"The Sukhoi Su-75, also known as Checkmate, is a fifth-generation lightweight stealth fighter aircraft developed by Sukhoi Company. Unveiled in July 2021, the Su-75 is designed to be a cost-effective alternative to the Su-57, with a focus on export markets. The aircraft features advanced avionics, including a digital fly-by-wire system and a glass cockpit, providing the pilot with enhanced situational awareness and ease of operation. The Su-75 also incorporates low observability features, such as a reduced radar cross-section and infrared signature, making it difficult to detect and track by enemy air defenses.With a top speed of Mach 1.8 and a range of up to 3,000 km, the Su-75 is a highly maneuverable and agile fighter, capable of performing air superiority and ground attack missions. "
     },
     {
-        key :10,
+        key :11,
         title: "Mig-21",
         pictureURL:"https://c4.wallpaperflare.com/wallpaper/607/183/660/fighter-pilot-the-mig-21-okb-mikoyan-and-gurevich-the-bbc-romania-hd-wallpaper-preview.jpg",
         content:"The Mikoyan-Gurevich MiG-21, introduced in the 1950s, is a renowned supersonic jet fighter aircraft that became one of the most produced combat aircraft in history. With a top speed exceeding Mach 2, the MiG-21 played a significant role in the Cold War era as a lightweight interceptor and multi-role fighter. Its sleek delta wing design and powerful engine made it highly maneuverable and capable of engaging enemy aircraft in dogfights. Equipped with cannons and missiles, it proved to be a formidable opponent in aerial combat. Despite its age, the MiG-21 remains in service with numerous air forces around the world, serving in various roles, including interceptor, trainer, and reconnaissance aircraft. "
     },
     {
-        key :11,
+        key :12,
         title: "Mig-23",
         pictureURL:"https://c4.wallpaperflare.com/wallpaper/61/155/811/mig-23-aircraft-military-aircraft-vehicle-wallpaper-preview.jpg",
         content:"The Mikoyan-Gurevich MiG-23, introduced in the 1970s, is a versatile and highly capable swing-wing fighter aircraft developed by the Soviet Union. With a top speed exceeding Mach 2.3, the MiG-23 excelled in both air-to-air and air-to-ground missions. Its swing-wing design provided enhanced performance at different speeds and altitudes, making it suitable for a wide range of combat scenarios. Equipped with radar and advanced avionics, it was capable of engaging enemy aircraft with missiles and cannons, as well as conducting ground attack missions with precision-guided munitions. Despite its complexity and maintenance requirements, the MiG-23 served in numerous air forces around the world and participated in various conflicts during the Cold War era."
     },
     {
-        key :12,
+        key :13,
         title: "Mig-27",
         pictureURL:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/MiG-27_%2812142027335%29.jpg/640px-MiG-27_%2812142027335%29.jpg",
         content:"The Mikoyan-Gurevich MiG-27, introduced in the 1970s, is a ground-attack aircraft developed by the Soviet Union and later Russia. With a top speed of approximately Mach 1.7, the MiG-27 excels in close air support and precision strike missions. Its rugged design and powerful engine enable it to operate in challenging environments and deliver accurate strikes against ground targets. Equipped with a variety of weapons, including cannons, bombs, and missiles, it is capable of engaging armored vehicles, fortifications, and other enemy targets with precision and firepower. Despite its age, the MiG-27 remains in service with several air forces worldwide, demonstrating its enduring effectiveness in modern warfare scenarios. "
     },
     {
-        key :13,
+        key :14,
         title: "Mig-29",
         pictureURL:"https://hips.hearstapps.com/hmg-prod/images/slovak-air-force-mig-29as-64177e184cf91.jpeg?crop=1.00xw:0.669xh;0,0.184xh&resize=640:*",
         content:"The Mikoyan MiG-29, introduced in the 1980s, is a renowned twin-engine jet fighter aircraft developed by the Soviet Union. With a top speed exceeding Mach 2.25, the MiG-29 excels in air superiority missions and aerial combat. Its sleek design and advanced avionics provide exceptional agility and maneuverability, allowing it to outmaneuver and engage enemy aircraft effectively. Equipped with a diverse array of weapons, including cannons, missiles, and bombs, it offers versatility in various combat scenarios. Despite its age, the MiG-29 remains in service with numerous air forces worldwide, showcasing its reliability and combat effectiveness in modern aerial warfare."
     },
     {
-        key :14,
+        key :15,
         title: "Mig-31",
         pictureURL:"https://i.insider.com/59a99fa16eac402a008b7e33?width=700",
         content:"The Mikoyan MiG-31, introduced in the 1980s, is a high-speed interceptor aircraft developed by the Soviet Union. With a top speed exceeding Mach 2.8, the MiG-31 excels in intercepting and engaging high-speed aerial targets, including aircraft and missiles. Its long-range radar and advanced avionics enable it to detect and track multiple targets simultaneously, while its powerful engines and heavy armament provide formidable firepower. Equipped with air-to-air missiles and cannons, it can effectively engage threats at extended ranges and in all weather conditions. Despite its primary role as an interceptor, the MiG-31 also possesses limited ground-attack capabilities, making it a versatile asset for air defense and homeland security. "
     },
     {
-        key :15,
+        key :16,
         title: "Mig-35",
         pictureURL:"https://images4.alphacoders.com/762/762228.jpg",
-        content:"The Mikoyan MiG-31, introduced in the 1980s, is a high-speed interceptor aircraft developed by the Soviet Union. With a top speed exceeding Mach 2.8, the MiG-31 excels in intercepting and engaging high-speed aerial targets, including aircraft and missiles. Its long-range radar and advanced avionics enable it to detect and track multiple targets simultaneously, while its powerful engines and heavy armament provide formidable firepower. Equipped with air-to-air missiles and cannons, it can effectively engage threats at extended ranges and in all weather conditions. Despite its primary role as an interceptor, the MiG-31 also possesses limited ground-attack capabilities, making it a versatile asset for air defense and homeland security. "
+        content:"The Mikoyan MiG-35, a contemporary multirole fighter aircraft, epitomizes Russia's commitment to cutting-edge aviation technology. Designed to excel in air superiority missions, it boasts advanced avionics, including state-of-the-art radar systems and sensor fusion technology, enhancing situational awareness and target detection capabilities. With its high-speed maneuverability and top speed exceeding Mach 2.0, the MiG-35 swiftly intercepts and engages both aerial and surface threats with precision. Its versatile arsenal, comprising advanced air-to-air and air-to-surface munitions, ensures formidable firepower for dominating the battlefield. The MiG-35's adaptability enables seamless transitions between air-to-air combat, ground attack missions, and reconnaissance tasks, making it a highly versatile asset in modern warfare scenarios."
     },
     
 
